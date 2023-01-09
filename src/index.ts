3450a95f1576c404a1fb7950351d08382b89ddbd
unknown <cleide.edificacoes@hotmail.com>
2023-01-09 00:34:39 -0300

    Projeto Finalizado 2

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,17 @@ import { createStudent } from "./endpoints/createStudent";
 import { createTeacher } from "./endpoints/createTeacher";
 import { student } from "./endpoints/student";
 import { teacher} from "./endpoints/teacher"
+import dotenv from "dotenv"
 
-
+dotenv.config()
 
 const app = express ();
 app.use(express.json());
 app.use(cors());
 
 
-app.listen(3003,()=> {
-    console.log("Servidor rodando na porta 3003");
+app.listen(process.env.PORT || 3003,()=> {
+    console.log(`Servidor rodando na porta ${process.env.PORT}`);
 })
 
 app.get("/ping", ping);
